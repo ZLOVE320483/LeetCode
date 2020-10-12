34fa405363606e42fab42419d5363a520450a30f
zlove.zhang <zlove.zhang@bytedance.com>
2020-10-12 22:37:37 +0800

    practice 4: isAnagram

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -3,11 +3,7 @@ package com.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        String text = "we will we will rock you";
-        FindOcurrences findOcurrences = new FindOcurrences();
-        String[] value = findOcurrences.findOcurrences(text, "we", "will");
-        for (int i = 0; i < value.length; i++) {
-            System.out.println(value[i]);
-        }
+        IsAnagram isAnagram = new IsAnagram();
+        System.out.println(isAnagram.isAnagram("anagram", "nagaram"));
     }
 }
