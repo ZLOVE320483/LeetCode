3bf19679c8b587debb8a899c2162a2dc858a941b
zlove.zhang <zlove.zhang@bytedance.com>
2020-10-17 08:51:55 +0800

    optimize knapsack

--- src/com/leetcode/Knapsack.java
+++ src/com/leetcode/Knapsack.java
@@ -5,17 +5,23 @@ public class Knapsack {
     public int getMaxValue(int[] w, int[] v, int capacity) {
         int wLen = w.length;
         int[][] dp = new int[wLen][capacity + 1];
-        for (int k = 1; k < wLen; k++) {
+        for (int k = 0; k < wLen; k++) {
             for (int c = 1; c <= capacity; c++) {
-                if (w[k] > c) {
-                    dp[k][c] = dp[k - 1][c];
+                if (k == 0) {
+                    if (dp[0][c] < v[k] && w[0] <= c) {
+                        dp[0][c] = v[k];
+                    }
                 } else {
-                    int value1 = dp[k - 1][c - w[k]] + v[k];
-                    int value2 = dp[k - 1][c];
-                    if (value1 > value2) {
-                        dp[k][c] = value1;
+                    if (w[k] > c) {
+                        dp[k][c] = dp[k - 1][c];
                     } else {
-                        dp[k][c] = value2;
+                        int value1 = dp[k - 1][c - w[k]] + v[k];
+                        int value2 = dp[k - 1][c];
+                        if (value1 > value2) {
+                            dp[k][c] = value1;
+                        } else {
+                            dp[k][c] = value2;
+                        }
                     }
                 }
                 System.out.println(String.format("dp[%d][%d]=%d", k, c, dp[k][c]));
--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -3,8 +3,8 @@ package com.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        int[] w = {0, 2, 3, 4, 5, 9};
-        int[] v = {0, 3, 4, 5, 8, 10};
+        int[] w = {2, 3, 4, 5, 9};
+        int[] v = {3, 4, 5, 8, 10};
         Knapsack knapsack = new Knapsack();
         System.out.println(knapsack.getMaxValue(w, v, 20));
     }
