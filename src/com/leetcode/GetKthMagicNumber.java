a90cac9873f8050dacfaca23173bc0391a0b8cba
zlove.zhang <zlove.zhang@bytedance.com>
2020-10-26 23:18:32 +0800

    practice 19: GetKthMagicNumber

---
+++ src/com/leetcode/GetKthMagicNumber.java
@@ -0,0 +1,26 @@
+package com.leetcode;
+
+public class GetKthMagicNumber {
+
+    public int getKthMagicNumber(int k) {
+       int[] res = new int[k];
+       res[0] = 1;
+       int index3 = 0, index5 = 0, index7 = 0;
+       int dp3 = 3, dp5 = 5, dp7 = 7;
+        for (int i = 1; i < k; i++) {
+            int min = Math.min(Math.min(res[index3] * dp3, res[index5] * dp5), res[index7] * dp7);
+            if (min == res[index3] * dp3) {
+                index3++;
+            }
+            if (min == res[index5] * dp5) {
+                index5++;
+            }
+            if (min == res[index7] * dp7) {
+                index7++;
+            }
+            res[i] = min;
+        }
+       return res[k - 1];
+    }
+
+}
--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -3,7 +3,7 @@ package com.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        NthUglyNumber nthUglyNumber = new NthUglyNumber();
-        System.out.println(nthUglyNumber.nthUglyNumber(10));
+        GetKthMagicNumber getKthMagicNumber = new GetKthMagicNumber();
+        System.out.println(getKthMagicNumber.getKthMagicNumber(5));
     }
 }
