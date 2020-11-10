8c9ed5b981d5d697c12b90258ca7337c7776bf9c
zlove.zhang <zlove.zhang@bytedance.com>
2020-11-10 17:04:25 +0800

    Practice 38: MaxWidthOfVerticalArea

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,13 +1,15 @@
 package com.leetcode;
 
-import com.leetcode.utils.PrintUtils;
-
 public class Main {
 
     public static void main(String[] args) {
-        NextPermutation permutation = new NextPermutation();
-        int[] nums = {3, 2, 1};
-        permutation.nextPermutation(nums);
-        PrintUtils.printIntArray(nums);
+        int[][] points = {
+                {8, 7},
+                {9, 9},
+                {7, 4},
+                {9, 7}
+        };
+        MaxWidthOfVerticalArea area = new MaxWidthOfVerticalArea();
+        System.out.println(area.maxWidthOfVerticalArea(points));
     }
 }
--- src/com/leetcode/NextPermutation.java
+++ src/com/leetcode/NextPermutation.java
@@ -1,7 +1,5 @@
 package com.leetcode;
 
-import java.util.Arrays;
-
 /**
  * https://leetcode-cn.com/problems/next-permutation/
  */
