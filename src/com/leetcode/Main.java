57f7fd6f591da1b9415d97db6e08e42fc125dbd0
zlove.zhang <zlove.zhang@bytedance.com>
2020-11-10 14:54:05 +0800

    Practice 37: NextPermutation

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -5,15 +5,9 @@ import com.leetcode.utils.PrintUtils;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] points = {
-                {3, 3},
-                {5, -1},
-                {-2, 4}
-        };
-        KClosest closest = new KClosest();
-        int[][] res = closest.kClosest(points, 2);
-        for (int i = 0; i < res.length; i++) {
-            PrintUtils.printIntArray(res[i]);
-        }
+        NextPermutation permutation = new NextPermutation();
+        int[] nums = {3, 2, 1};
+        permutation.nextPermutation(nums);
+        PrintUtils.printIntArray(nums);
     }
 }
---
+++ src/com/leetcode/NextPermutation.java
@@ -0,0 +1,39 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/next-permutation/
+ */
+public class NextPermutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    private void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
