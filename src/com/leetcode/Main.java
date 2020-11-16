49a94ffb557dd5a7cbecb4d945542d308e19719e
zlove.zhang <zlove.zhang@bytedance.com>
2020-11-16 17:05:04 +0800

    Practice 46: Divider

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -3,8 +3,7 @@ package com.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr1 = {9,8,7,6,5,1,2,3,4};
-        MaxCoins coins = new MaxCoins();
-        System.out.println(coins.maxCoins(arr1));
+        Divide divide = new Divide();
+        System.out.println(divide.divide(-2147483648, -1));
     }
 }
