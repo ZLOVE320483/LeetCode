3d5ddb1246fe77d2a4d72c76b050ee2dd6d71ce8
zlove.zhang <zlove.zhang@bytedance.com>
2020-11-17 14:12:55 +0800

    Practice 47: AllCellsDistOrder

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,9 +1,14 @@
 package com.leetcode;
 
+import com.leetcode.utils.PrintUtils;
+
 public class Main {
 
     public static void main(String[] args) {
-        Divide divide = new Divide();
-        System.out.println(divide.divide(-2147483648, -1));
+        AllCellsDistOrder order = new AllCellsDistOrder();
+        int[][] res = order.allCellsDistOrder(3, 4, 1, 1);
+        for (int i = 0; i < res.length; i++) {
+            PrintUtils.printIntArray(res[i]);
+        }
     }
 }
