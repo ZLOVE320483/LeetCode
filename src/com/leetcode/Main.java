de8f83864a78bce3b518ce3abcc76da869c7cb7b
zlove.zhang <zlove.zhang@bytedance.com>
2020-12-11 20:01:41 +0800

    Practice: Sort

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,13 +1,13 @@
 package com.leetcode;
 
-import com.leetcode.sort_test.InsertSort;
+import com.leetcode.sort_test.ShellSort;
 import com.leetcode.utils.PrintUtils;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] nums = {1, 0, -1, 0, -2, 2, 5, 7, 9, 8};
-        InsertSort.sort(nums);
+        ShellSort.sort(nums);
         PrintUtils.printIntArray(nums);
     }
 }
