74b7de9fdd9124a9c6ab9f6763c5f11ca207b823
zlove.zhang <zlove.zhang@bytedance.com>
2020-12-12 20:49:56 +0800

    Practice: MergeSort

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,13 +1,12 @@
 package com.leetcode;
 
-import com.leetcode.sort_test.ShellSort;
+import com.leetcode.sort_test.MergeSort;
 import com.leetcode.utils.PrintUtils;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] nums = {1, 0, -1, 0, -2, 2, 5, 7, 9, 8};
-        ShellSort.sort(nums);
-        PrintUtils.printIntArray(nums);
+        PrintUtils.printIntArray(MergeSort.sort(nums));
     }
 }
---
+++ src/com/leetcode/sort_test/MergeSort.java
@@ -0,0 +1,41 @@
+package com.leetcode.sort_test;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static int[] sort(int[] array) {
+        if (array.length < 2)
+            return array;
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+        return merge(sort(left), sort(right));
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] res = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                res[k] = left[i];
+                i++;
+            } else {
+                res[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            res[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            res[k] = right[j];
+            j++;
+            k++;
+        }
+        return res;
+    }
+}
