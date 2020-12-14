00f042d38b5bff607cd52f3c2ed221850fea9044
zlove.zhang <zlove.zhang@bytedance.com>
2020-12-14 15:32:48 +0800

    Practice: isValidBST

---
+++ src/com/leetcode/tree/bst/IsValidBST.java
@@ -0,0 +1,22 @@
+package com.leetcode.tree.bst;
+
+import com.leetcode.tree.TreeNode;
+
+public class IsValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.val <= min.val)
+            return false;
+        if (max != null && root.val >= max.val)
+            return false;
+        return isValidBST(root.left, min, root)
+                && isValidBST(root.right, root, max);
+    }
+}
