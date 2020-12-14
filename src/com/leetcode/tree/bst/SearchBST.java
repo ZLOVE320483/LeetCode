23bc291521e66117120ccd1288f33b63b2fb99a3
zlove.zhang <zlove.zhang@bytedance.com>
2020-12-14 19:57:19 +0800

    Practice: SearchBST

---
+++ src/com/leetcode/tree/bst/SearchBST.java
@@ -0,0 +1,20 @@
+package com.leetcode.tree.bst;
+
+import com.leetcode.tree.TreeNode;
+
+public class SearchBST {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
