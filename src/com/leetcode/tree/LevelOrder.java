430e6b3c8e1309da8bdeb3114239a82d55b115ed
zlove.zhang <zlove.zhang@bytedance.com>
2020-12-15 17:41:06 +0800

    Practice: Level Order

---
+++ src/com/leetcode/tree/LevelOrder.java
@@ -0,0 +1,34 @@
+package com.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int currentLevelSize = queue.size();
+            List<Integer> level = new LinkedList<>();
+            for (int i = 1; i <= currentLevelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
