8d8f5dcee56b038c83726247a800526f6d0add2e
zlove.zhang <zlove.zhang@bytedance.com>
2020-12-17 19:41:45 +0800

    Practice: InsertIntoBST

---
+++ src/com/leetcode/tree/bst/InsertIntoBST.java
@@ -0,0 +1,17 @@
+package com.leetcode.tree.bst;
+
+import com.leetcode.tree.TreeNode;
+
+public class InsertIntoBST {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val < val)
+            root.right = insertIntoBST(root.right, val);
+        if (root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        return root;
+    }
+}
