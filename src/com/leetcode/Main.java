b1b812a6075f24620c5da168aec31fde9c4da461
zlove.zhang <zlove.zhang@bytedance.com>
2021-01-05 15:36:02 +0800

    Multiply

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -1,12 +1,9 @@
 package com.leetcode;
 
-import com.leetcode.sort.FindKthLargest;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {3, 2, 1, 5, 6, 4};
-        FindKthLargest largest = new FindKthLargest();
-        System.out.println(largest.findKthLargest(nums, 2));
+        Multiply multiply = new Multiply();
+        System.out.println(multiply.multiply("0", "0"));
     }
 }
---
+++ src/com/leetcode/Multiply.java
@@ -0,0 +1,30 @@
+package com.leetcode;
+
+public class Multiply {
+
+    public String multiply(String num1, String num2) {
+        int m = num1.length(), n = num2.length();
+        int[] res = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int multi = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = multi + res[p2];
+                res[p2] = sum % 10;
+                res[p1] += sum / 10;
+            }
+        }
+        int i = 0;
+        while (i < res.length && res[i] == 0) {
+            i++;
+        }
+        if (i >= res.length) {
+            return "0";
+        }
+        StringBuilder builder = new StringBuilder();
+        for (;i < res.length; i++) {
+            builder.append(res[i]);
+        }
+        return builder.toString();
+    }
+}
