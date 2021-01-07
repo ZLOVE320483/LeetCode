5a2f4ba543520dfd3e24ad360f5f4e5290b2c357
zlove.zhang <zlove.zhang@bytedance.com>
2021-01-07 15:01:34 +0800

    LongestPalindrome

--- src/com/leetcode/Main.java
+++ src/com/leetcode/Main.java
@@ -3,7 +3,7 @@ package com.leetcode;
 public class Main {
 
     public static void main(String[] args) {
-        CalculateII calculate = new CalculateII();
-        System.out.println(calculate.calculate("1 + 2 - (3 + 5) - 100 / 4"));
+        LongestPalindrome palindrome = new LongestPalindrome();
+        System.out.println(palindrome.longestPalindrome("babad"));
     }
 }
