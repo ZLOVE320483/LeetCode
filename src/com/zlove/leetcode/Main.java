d9b195a5299dfd9faaed1a32db3dd9b34acbe3eb
zlove.zhang <zlove.zhang@bytedance.com>
2021-01-20 17:25:53 +0800

    Binary Search

--- src/com/zlove/leetcode/Main.java
+++ src/com/zlove/leetcode/Main.java
@@ -1,12 +1,11 @@
 package com.zlove.leetcode;
 
-import com.zlove.leetcode.bfs.OpenLock;
+import com.zlove.leetcode.binary_search.BinarySearch;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] deadends = {"0201","0101","0102","1212","2002"};
-        String target = "0202";
-        System.out.println(OpenLock.openLock(deadends, target));
+        int[] nums = {-1,0,3,5,9,12};
+        System.out.println(BinarySearch.binarySearch(nums, 1));
     }
 }
