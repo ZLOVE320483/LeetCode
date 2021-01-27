24eab46739cbd88f1fa89d55a979b8a8e377467a
zlove.zhang <zlove.zhang@bytedance.com>
2021-01-27 17:04:04 +0800

    CheckInclusion

--- src/com/zlove/leetcode/window/CheckInclusion.java
+++ src/com/zlove/leetcode/window/CheckInclusion.java
@@ -1,17 +1,14 @@
-package com.leetcode.window;
+package com.zlove.leetcode.window;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class CheckInclusion {
 
-    public boolean checkInclusion(String s1, String s2) {
-
-        // 存储 s1 串的信息， key-value 对应的是每个字符和该字符的个数
+    public static boolean checkInclusion(String s1, String s2) {
         Map<Character, Integer> needs = new HashMap<>();
-
-        // 遍历 s2 串，存储滑动窗口的信息
         Map<Character, Integer> window = new HashMap<>();
+
         for (Character c : s1.toCharArray()) {
             if (needs.containsKey(c)) {
                 needs.put(c, needs.get(c) + 1);
@@ -21,8 +18,6 @@ public class CheckInclusion {
         }
 
         int left = 0, right = 0;
-
-        // 窗口中 字符个数与 needs 中字符个数相等时的数量
         int valid = 0;
 
         while (right < s2.length()) {
@@ -34,10 +29,11 @@ public class CheckInclusion {
                 } else {
                     window.put(c, 1);
                 }
-                if (window.get(c).equals(needs.get(c))) {
+                if (needs.get(c).equals(window.get(c))) {
                     valid++;
                 }
             }
+
             while (right - left >= s1.length()) {
                 if (valid == needs.size()) {
                     return true;
@@ -45,13 +41,14 @@ public class CheckInclusion {
                 Character d = s2.charAt(left);
                 left++;
                 if (needs.containsKey(d)) {
-                    if (window.get(d).equals(needs.get(d))) {
+                    if (needs.get(d).equals(window.get(d))) {
                         valid--;
                     }
                     window.put(d, window.get(d) - 1);
                 }
             }
         }
+
         return false;
     }
 }
