c9d17229467327e9387a12ec8bd369063c8363f3
zlove.zhang <zlove.zhang@bytedance.com>
2021-02-22 16:13:39 +0800

    feat: merge sort

--- src/com/zlove/leetcode/Main.java
+++ src/com/zlove/leetcode/Main.java
@@ -1,13 +1,13 @@
 package com.zlove.leetcode;
 
 import com.leetcode.utils.PrintUtils;
-import com.zlove.leetcode.sort.QuickSort;
+import com.zlove.leetcode.sort.MergeSort;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] nums = {13,0,3,2,87,12};
-        QuickSort.quickSort(nums, 0, nums.length - 1);
-        PrintUtils.printIntArray(nums);
+        int[] res = MergeSort.sort(nums);
+        PrintUtils.printIntArray(res);
     }
 }
