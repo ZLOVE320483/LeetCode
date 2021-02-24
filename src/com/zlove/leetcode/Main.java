ee2126c4768c4328b33f4bac9f0228cc7426765c
zlove.zhang <zlove.zhang@bytedance.com>
2021-02-24 18:29:22 +0800

    thirtysix add

--- src/com/zlove/leetcode/Main.java
+++ src/com/zlove/leetcode/Main.java
@@ -1,5 +1,6 @@
 package com.zlove.leetcode;
 
+import com.leetcode.sort.FindKthLargest;
 import com.leetcode.utils.PrintUtils;
 import com.zlove.leetcode.sort.MergeSort;
 
@@ -7,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] nums = {13,0,3,2,87,12};
-        int[] res = MergeSort.sort(nums);
-        PrintUtils.printIntArray(res);
+        FindKthLargest largest = new FindKthLargest();
+        System.out.println(largest.findKthLargest(nums, 4));
     }
 }
