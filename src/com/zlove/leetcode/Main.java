5c2e20f068e96122c16129b9a48445cc94001e8d
zlove.zhang <zlove.zhang@bytedance.com>
2021-02-25 15:35:02 +0800

    feat: shell sort

--- src/com/zlove/leetcode/Main.java
+++ src/com/zlove/leetcode/Main.java
@@ -1,14 +1,14 @@
 package com.zlove.leetcode;
 
-import com.leetcode.sort.FindKthLargest;
+
 import com.leetcode.utils.PrintUtils;
-import com.zlove.leetcode.sort.MergeSort;
+import com.zlove.leetcode.sort.ShellSort;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] nums = {13,0,3,2,87,12};
-        FindKthLargest largest = new FindKthLargest();
-        System.out.println(largest.findKthLargest(nums, 4));
+        ShellSort.sort(nums);
+        PrintUtils.printIntArray(nums);
     }
 }
---
+++ src/com/zlove/leetcode/sort/ShellSort.java
@@ -0,0 +1,21 @@
+package com.zlove.leetcode.sort;
+
+public class ShellSort {
+
+    public static void sort(int[] nums) {
+        int length = nums.length;
+        int gap = length / 2;
+        while (gap > 0) {
+            for (int i = gap; i < length; i++) {
+                int tmp = nums[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && nums[preIndex] > tmp) {
+                    nums[preIndex + gap] = nums[preIndex];
+                    preIndex -= gap;
+                }
+                nums[preIndex + gap] = tmp;
+            }
+            gap /= 2;
+        }
+    }
+}
