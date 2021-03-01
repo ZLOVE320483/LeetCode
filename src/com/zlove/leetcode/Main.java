0a717f489b9a7ffeb72092ab23a403aae766673f
zlove.zhang <zlove.zhang@bytedance.com>
2021-03-01 17:45:55 +0800

    feat: heap sort

--- src/com/zlove/leetcode/Main.java
+++ src/com/zlove/leetcode/Main.java
@@ -2,13 +2,13 @@ package com.zlove.leetcode;
 
 
 import com.leetcode.utils.PrintUtils;
-import com.zlove.leetcode.sort.ShellSort;
+import com.zlove.leetcode.sort.HeapSort;
 
 public class Main {
 
     public static void main(String[] args) {
         int[] nums = {13,0,3,2,87,12};
-        ShellSort.sort(nums);
+        HeapSort.sort(nums);
         PrintUtils.printIntArray(nums);
     }
 }
